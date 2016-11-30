e9104b17f751c63902c19895e55a9709aaffe20b
PatrykPasterny <patryk.pasterny@gmail.com>
2016-11-30 12:47:20 +0100

    poprawa_znow

--- test_kol2_gr2.py
+++ test_kol2_gr2.py
@@ -5,26 +5,40 @@ class test_Szkola(unittest.TestCase):
 	def setUp(self):
 		self.Szkola=Szkola()
 		self.Klasa1=Klasa()
+		self.Klasa2=Klasa()
 		self.s1=student([2., 3.],"adam","malysz",40)
 		self.s2=student([4., 2.],"john","snow",70)
 		self.s3=student([5. ,4.],"donald","trump",10)
 		self.s4=student([5. ,5.],"ryszard","zklanu",20)
-		self.Klasa1.add_student(s1)
-		self.Klasa1.add_student(s2)
-		self.Klasa2.add_student(s3)
-		self.Klasa2.add_student(s4)
-		self.Szkola.add_class(k1)
-		self.Szkola.add_class(k2)
+		self.Klasa1.add_student(self.s1)
+		self.Klasa1.add_student(self.s2)
+		self.Klasa2.add_student(self.s3)
+		self.Klasa2.add_student(self.s4)
+		self.Szkola.add_class(self.Klasa1)
+		self.Szkola.add_class(self.Klasa2)
+		self.scores=[2.,3.]
+		self.name="adam"
+		self.surname="malysz"
+		self.attendance=70
+		self.test_instance_student = student(self.scores, self.name, self.surname,self.attendance)
+	
+	def test_init_student(self):
+		self.assertEqual(self.test_instance_student.name, self.name)
+		self.assertEqual(self.test_instance_student.surname, self.surname)
+		self.assertEqual(self.test_instance_student.scores, self.scores)
+		self.assertEqual(self.test_instance_student.attendance, self.attendance)
+	
 		
 	def test_get_totalaverage_in_school(self):
 		self.assertAlmostEqual(3.75,self.Szkola.get_totalaverage(),places=4)
 	
-	def test_get_totalaverage_in_class(self, Klasa1):
-		self.assertAlmostEqual(2.75,self.Szkola.get_totalaverageinclass(),places=4)
+	def test_get_totalaverage_in_class(self):
+		self.assertAlmostEqual(2.75,self.Szkola.get_totalaverageinclass(self.Klasa1),places=4)
 	
 	def test_get_attendance_in_school(self):
-		self.asserEqual(53,self.Szkola.get_attendance())	
+		self.assertEqual(35,self.Szkola.get_attendance())	
 
+	
 if __name__ == "__main__":
     unittest.main()			
 		
